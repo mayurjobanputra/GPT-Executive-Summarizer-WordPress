860d42f48f43705aa16f7ce0c90944f01bd4d744
Mayur <6332663+mayurjobanputra@users.noreply.github.com>
2023-12-20 18:30:13 -0800

    building mare betal

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# GPT-Executive-Summarizer-WordPress
+# WordPress GPT Executive Summarizer using OpenAI 
+
 This plugin, on a weekly schedule, creates an executive summary of your profile based on all the published Wordpress posts from your blog. It uses OpenAI to generate this one-pager. You can configure prompts, number of posts, schedule, and more.
---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: WordPress GPT Executive Summarizer using OpenAI
+Description: Assemble all published posts into a single text file, send to ChatGPT for summary, and update a WP configuration.
+Version: 1.0
+Author: Your Name
+*/
+
+// Hook for adding admin menus
+add_action('admin_menu', 'gpt_executive_summary_menu');
+
+// Action function for above hook
+function gpt_executive_summary_menu() {
+    add_options_page('GPT Executive Summary Settings', 'GPT Executive Summary', 'manage_options', 'gpt-executive-summary', 'gpt_executive_summary_settings_page');
+}
+
+// GPT Executive Summary Settings Page
+function gpt_executive_summary_settings_page() {
+    // Check user capabilities
+    if (!current_user_can('manage_options')) {
+        return;
+    }
+
+    // Save settings logic here
+
+    // Settings form
+    ?>
+    <div class="wrap">
+        <h1><?= esc_html(get_admin_page_title()); ?></h1>
+        <form action="options.php" method="post">
+            <!-- Here will be the settings fields -->
+        </form>
+    </div>
+    <?php
+}
+
+// Function to gather posts and create a text file
+function gather_posts_and_create_file() {
+    // Gather all published posts code here
+
+    // Add user-customized prompt at the top
+
+    // Save to text file
+}
+
+// Function to send text file to ChatGPT and get summary
+function get_summary_from_chatgpt($textFile) {
+    // OpenAI API request code here
+
+    // Return summary
+}
+
+// Function to update WP configuration with the summary
+function update_gpt_executive_summary($summary) {
+    // Update configuration code here
+}
+
+// Run the plugin logic
+function run_gpt_executive_summary() {
+    $textFile = gather_posts_and_create_file();
+    $summary = get_summary_from_chatgpt($textFile);
+    update_gpt_executive_summary($summary);
+}
+
+// Trigger the main function - this can be modified to be triggered in different ways (e.g., a button in admin panel)
+run_gpt_executive_summary();
+?>
